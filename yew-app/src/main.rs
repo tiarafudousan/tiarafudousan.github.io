af49a843790c390c1b46e3c703ff7eeeb024552b
t4sk <tsk.nakamura@gmail.com>
2022-11-01 08:58:27 +0900

    form

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -40,15 +40,40 @@ impl Component for App {
         // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
         let link = ctx.link();
         html! {
-            <div>
+            <div class="flex flex-col px-2 bg-gray-100">
                 <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
                 <p>{ self.value }</p>
                 <form>
-                <div>{"物件情報"}</div>
-                    <input />
-                    <input />
-                    <input />
-                    <input />
+                    <div class="flex flex-col">
+                        <div class="text-lg">{"物件情報"}</div>
+
+                        <div class="bg-red-100 space-y-2 py-2 px-3">
+                            <div class="flex flex-row items-center">
+                                <label class="w-[130px]">{"物件価格"}</label>
+                                <input class="w-[70px] px-2 py-0.5 text-right border border-gray-100 rounded-sm" />
+                                <div class="ml-1">{"万円"}</div>
+                            </div>
+
+                            <div class="flex flex-row">
+                                <label class="w-[130px]">{"満室時想定年収"}</label>
+                                <input />
+                                <div>{"万円"}</div>
+                            </div>
+                            
+                            <div class="flex flex-row">
+                                <label class="w-[130px]">{"想定空室率"}</label>
+                                <input />
+                                <div>{"%"}</div>
+                            </div>
+
+                            <div class="flex flex-row">
+                                <label class="w-[130px]">{"諸経費率"}</label>
+                                <input />
+                                <div>{"%"}</div>
+                            </div>
+                        </div>
+                    </div>
+
                 <div>{"資金計画"}</div>
                     <input />
                     <input />
