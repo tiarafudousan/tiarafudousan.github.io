c67e3e2ba7c14971de157dbf6d0b953a3048049c
t4sk <tsk.nakamura@gmail.com>
2022-11-07 08:52:25 +0900

    input

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -7,44 +7,28 @@ use yew::prelude::*;
 //     }
 // }
 
-use yew::{Component, Context, html, Html, Properties, Children};
-
-enum MyMsg {
-    Click,
-}
-
-#[derive(PartialEq, Properties)]
-struct Props {
-    // children: Children,
-    // text: String,
+#[derive(Properties, PartialEq)]
+struct InputProps {
     label: String,
     unit: String,
     #[prop_or_default]
     width: Option<u32>
 }
 
-struct Input;
-
-impl Component for Input {
-    type Message = MyMsg;
-    type Properties = Props;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // let onclick = ctx.link().callback(|_| MyMsg::Click);
-        let width = &ctx.props().width.unwrap_or(300);
-        html! {
-            <div class="flex flex-row items-center">
-                <label class="w-[130px]">{&ctx.props().label}</label>
-                <input class={format!("w-[{width}px] px-2 py-0.5 text-right border border-gray-100 rounded-sm")} />
-                <div class="ml-1">{&ctx.props().unit}</div>
-            </div>
-        }
+#[function_component(Input)]
+fn input(props: &InputProps) -> Html {
+    let width = &props.width.unwrap_or(300);
+    html! {
+        <div class="flex flex-row items-center">
+            <label class="w-[130px]">{&props.label}</label>
+            <input class={format!("w-[{width}px] px-2 py-0.5 text-right border border-gray-100 rounded-sm")} />
+            <div class="ml-1">{&props.unit}</div>
+        </div>
     }
 }
+
+use yew::{function_component, Component, Context, html, Html, Properties, Children};
+
 
 enum Msg {
     AddOne,
@@ -89,18 +73,8 @@ impl Component for App {
                         <div class="bg-red-100 space-y-2 py-2 px-3">
                             <Input label="物件価格" unit="万円" width={Some(70)}/>
                             <Input label="満室時想定年収" unit="万円" width={Some(70)}/>
-
-                            <div class="flex flex-row">
-                                <label class="w-[130px]">{"想定空室率"}</label>
-                                <input />
-                                <div>{"%"}</div>
-                            </div>
-
-                            <div class="flex flex-row">
-                                <label class="w-[130px]">{"諸経費率"}</label>
-                                <input />
-                                <div>{"%"}</div>
-                            </div>
+                            <Input label="想定空室率" unit="%" width={Some(70)}/>
+                            <Input label="諸経費率" unit="%" width={Some(70)}/>
                         </div>
                     </div>
 
