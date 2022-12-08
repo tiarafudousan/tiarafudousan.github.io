25159653d451b1e0e2d41ecba847cedb780586be
t4sk <tsk.nakamura@gmail.com>
2022-12-09 08:39:31 +0900

    input

--- app/src/components/Input.tsx
+++ app/src/components/Input.tsx
@@ -1,29 +1,51 @@
 import React from "react"
 
+function append(c1: string, c2: string, bool: boolean) {
+  if (bool) {
+    return `${c1} ${c2}`
+  }
+  return c1
+}
+
 interface Props {
   label: string
   unit: string
   name: string
   value: string
   onChange: (name: string, value: string) => void
+  error?: string
 }
 
-const Input: React.FC<Props> = ({ label, unit, name, value, onChange }) => {
+const Input: React.FC<Props> = ({
+  label,
+  unit,
+  name,
+  value,
+  onChange,
+  error,
+}) => {
   function _onChange(e: React.ChangeEvent<HTMLInputElement>) {
     onChange(name, e.target.value)
   }
 
   return (
-    <div className="flex flex-row items-center py-1">
-      <label className="w-[120px] mr-2">{label}</label>
-      <input
-        className="w-[100px] px-2 py-0.5 text-right border border-gray-100 focus:outline-none focus:ring focus:ring-blue-200 rounded-sm"
-        type="number"
-        min={0}
-        value={value}
-        onChange={_onChange}
-      />
-      <div className="ml-2">{unit}</div>
+    <div className="flex flex-col">
+      <div className="flex flex-row items-center py-1">
+        <label className="w-[120px] mr-2">{label}</label>
+        <input
+          className={append(
+            "w-[100px] px-2 py-0.5 text-right border border-gray-100 focus:outline-none focus:ring focus:ring-blue-200 rounded-sm",
+            "bg-red-100 border-red-200",
+            !!error
+          )}
+          type="number"
+          min={0}
+          value={value}
+          onChange={_onChange}
+        />
+        <div className="ml-2">{unit}</div>
+      </div>
+      <div className="text-sm text-red-500 h-[20px]">{error}</div>
     </div>
   )
 }
