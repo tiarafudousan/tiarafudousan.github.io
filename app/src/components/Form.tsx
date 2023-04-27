abcb6bdaa12f31787e79ddffed6dabcb2a1d9105
t4sk <tsk.nakamura@gmail.com>
2023-04-27 14:52:30 +0900

    form - purchase cost

--- app/src/components/Form.tsx
+++ app/src/components/Form.tsx
@@ -1,145 +1,164 @@
 import React, { useState } from "react"
-import { Inputs, Errors, validate } from "../lib/form"
+import { Inputs, Errors, calculateLoan, validate } from "../lib/form"
 import Input from "./Input"
 
 const INPUTS: Inputs<string> = {
-  property_price: "2000",
-  yearly_income: "200",
-  vacancy_rate: "15",
-  running_cost_rate: "20",
-  cash: "0",
-  loan: "2000",
-  years: "10",
-  interest_rate: "3",
+    property_price: "2000",
+    yearly_income: "200",
+    vacancy_rate: "15",
+    running_cost_rate: "20",
+    cash: "0",
+    purchase_cost: "0",
+    loan: "2000",
+    years: "10",
+    interest_rate: "3",
 }
 
 interface Props {
-  onSubmit: (values: Inputs<number>) => void
-  onReset: () => void
+    onSubmit: (values: Inputs<number>) => void
+    onReset: () => void
 }
 
 const Form: React.FC<Props> = ({ onSubmit, onReset }) => {
-  const [inputs, setInputs] = useState<Inputs<string>>(INPUTS)
-  const [errors, setErrors] = useState<Errors>({})
+    const [inputs, setInputs] = useState<Inputs<string>>(INPUTS)
+    const [errors, setErrors] = useState<Errors>({})
 
-  function onChange(name: string, value: string) {
-    setInputs((inputs) => ({
-      ...inputs,
-      [name]: value,
-    }))
-  }
+    function onChange(name: string, value: string) {
+        setInputs((inputs) => {
+            const newInputs = {
+                ...inputs,
+                [name]: value,
+            }
 
-  function _onSubmit(
-    e:
-      | React.MouseEvent<HTMLButtonElement, MouseEvent>
-      | React.FormEvent<HTMLFormElement>
-  ) {
-    e.preventDefault()
-    setErrors({})
+            const loan = calculateLoan(newInputs)
 
-    const [errors, values] = validate(inputs)
-
-    if (errors) {
-      setErrors(errors)
-      return
+            return {
+                ...newInputs,
+                loan: loan != null ? loan.toString() : "",
+            }
+        })
     }
 
-    onSubmit(values)
-  }
+    function _onSubmit(
+        e:
+            | React.MouseEvent<HTMLButtonElement, MouseEvent>
+            | React.FormEvent<HTMLFormElement>
+    ) {
+        e.preventDefault()
+        setErrors({})
 
-  function _onReset(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    e.preventDefault()
-    setInputs(INPUTS)
-    setErrors({})
-    onReset()
-  }
-  return (
-    <form onSubmit={_onSubmit}>
-      <h1 className="text-xl font-bold">物件情報</h1>
-      <Input
-        label="物件価格"
-        unit="万円"
-        name="property_price"
-        value={inputs.property_price}
-        onChange={onChange}
-        error={errors?.property_price}
-      />
-      <Input
-        label="満室年収"
-        unit="万円"
-        name="yearly_income"
-        value={inputs.yearly_income}
-        onChange={onChange}
-        error={errors?.property_price}
-      />
-      <Input
-        label="空室率"
-        unit="%"
-        name="vacancy_rate"
-        value={inputs.vacancy_rate}
-        onChange={onChange}
-        error={errors?.vacancy_rate}
-      />
-      <Input
-        label="諸経費率"
-        unit="%"
-        name="running_cost_rate"
-        value={inputs.running_cost_rate}
-        onChange={onChange}
-        error={errors?.running_cost_rate}
-      />
+        const [errors, values] = validate(inputs)
 
-      <h1 className="text-xl font-bold">資金計画</h1>
-      <Input
-        label="自己資金"
-        unit="万円"
-        name="cash"
-        value={inputs.cash}
-        onChange={onChange}
-        error={errors?.cash}
-      />
-      <Input
-        label="借入金額"
-        unit="万円"
-        name="loan"
-        value={inputs.loan}
-        onChange={onChange}
-        error={errors?.loan}
-      />
-      <Input
-        label="借入期間"
-        unit="年"
-        name="years"
-        value={inputs.years}
-        onChange={onChange}
-        error={errors?.years}
-      />
-      <Input
-        label="借入金利"
-        unit="%"
-        step={0.01}
-        name="interest_rate"
-        value={inputs.interest_rate}
-        onChange={onChange}
-        error={errors?.interest_rate}
-      />
+        if (errors) {
+            setErrors(errors)
+            return
+        }
 
-      <div className="flex flex-row justify-center space-x-2 mt-4">
-        <button
-          className="px-5 py-2 rounded-lg bg-green-600 hover:bg-green-500 text-white transition ease-in-out duration-150 focus:outline-none focus:ring-2 focus:ring-green-300"
-          onClick={(e) => onSubmit}
-        >
-          計算
-        </button>
-        <button
-          className="px-5 py-2 rounded-lg text-gray-900 bg-white hover:bg-gray-100 border border-gray-300 transition ease-in-out duration-150 focus:outline-none focus:ring-2 focus:ring-gray-300"
-          onClick={_onReset}
-        >
-          リセット
-        </button>
-      </div>
-    </form>
-  )
+        onSubmit(values)
+    }
+
+    function _onReset(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        e.preventDefault()
+        setInputs(INPUTS)
+        setErrors({})
+        onReset()
+    }
+
+    return (
+        <form onSubmit={_onSubmit}>
+            <h1 className="text-xl font-bold">物件情報</h1>
+            <Input
+                label="物件価格"
+                unit="万円"
+                name="property_price"
+                value={inputs.property_price}
+                onChange={onChange}
+                error={errors?.property_price}
+            />
+            <Input
+                label="満室年収"
+                unit="万円"
+                name="yearly_income"
+                value={inputs.yearly_income}
+                onChange={onChange}
+                error={errors?.property_price}
+            />
+            <Input
+                label="空室率"
+                unit="%"
+                name="vacancy_rate"
+                value={inputs.vacancy_rate}
+                onChange={onChange}
+                error={errors?.vacancy_rate}
+            />
+            <Input
+                label="諸経費率"
+                unit="%"
+                name="running_cost_rate"
+                value={inputs.running_cost_rate}
+                onChange={onChange}
+                error={errors?.running_cost_rate}
+            />
+
+            <h1 className="text-xl font-bold">資金計画</h1>
+            <Input
+                label="自己資金"
+                unit="万円"
+                name="cash"
+                value={inputs.cash}
+                onChange={onChange}
+                error={errors?.cash}
+            />
+            <Input
+                label="購入時諸費用"
+                unit="万円"
+                name="purchase_cost"
+                value={inputs.purchase_cost}
+                onChange={onChange}
+                error={errors?.purchase_cost}
+            />
+            <Input
+                label="借入金額"
+                unit="万円"
+                name="loan"
+                value={inputs.loan}
+                error=""
+                disabled={true}
+            />
+            <Input
+                label="借入期間"
+                unit="年"
+                name="years"
+                value={inputs.years}
+                onChange={onChange}
+                error={errors?.years}
+            />
+            <Input
+                label="借入金利"
+                unit="%"
+                step={0.01}
+                name="interest_rate"
+                value={inputs.interest_rate}
+                onChange={onChange}
+                error={errors?.interest_rate}
+            />
+
+            <div className="flex flex-row justify-center space-x-2 mt-4">
+                <button
+                    className="px-5 py-2 rounded-lg bg-green-600 hover:bg-green-500 text-white transition ease-in-out duration-150 focus:outline-none focus:ring-2 focus:ring-green-300"
+                    onClick={(e) => onSubmit}
+                >
+                    計算
+                </button>
+                <button
+                    className="px-5 py-2 rounded-lg text-gray-900 bg-white hover:bg-gray-100 border border-gray-300 transition ease-in-out duration-150 focus:outline-none focus:ring-2 focus:ring-gray-300"
+                    onClick={_onReset}
+                >
+                    リセット
+                </button>
+            </div>
+        </form>
+    )
 }
 
 export default Form
--- app/src/lib/form.ts
+++ app/src/lib/form.ts
@@ -1,134 +1,160 @@
 export interface Inputs<a> {
-  property_price: a
-  yearly_income: a
-  vacancy_rate: a
-  running_cost_rate: a
-  cash: a
-  loan: a
-  years: a
-  interest_rate: a
+    property_price: a
+    yearly_income: a
+    vacancy_rate: a
+    running_cost_rate: a
+    cash: a
+    purchase_cost: a
+    loan: a
+    years: a
+    interest_rate: a
 }
 
 export interface Errors {
-  property_price?: string
-  yearly_income?: string
-  vacancy_rate?: string
-  running_cost_rate?: string
-  cash?: string
-  loan?: string
-  years?: string
-  interest_rate?: string
+    property_price?: string
+    yearly_income?: string
+    vacancy_rate?: string
+    running_cost_rate?: string
+    cash?: string
+    purchase_cost?: string
+    years?: string
+    interest_rate?: string
 }
 
 function validateNum(
-  input: string,
-  int: boolean = true,
-  min = 0,
-  max = Infinity
+    input: string,
+    int: boolean = true,
+    min = 0,
+    max = Infinity
 ): [error: string | null, value: number] {
-  let error = null
-  const value = int ? parseInt(input) : parseFloat(input)
+    let error = null
+    const value = int ? parseInt(input) : parseFloat(input)
 
-  if (isNaN(value)) {
-    error = "数字を入力してください"
-  } else if (value < min) {
-    error = `${min}より大きい数字を入力してください`
-  } else if (value > max) {
-    error = `${max}より小さい数字を入力してください`
-  }
+    if (isNaN(value)) {
+        error = "数字を入力してください"
+    } else if (value < min) {
+        error = `${min}より大きい数字を入力してください`
+    } else if (value > max) {
+        error = `${max}より小さい数字を入力してください`
+    }
 
-  return [error, value]
+    return [error, value]
 }
 
 export function validate(
-  inputs: Inputs<string>
+    inputs: Inputs<string>
 ): [errors: Errors | null, values: Inputs<number>] {
-  const values: Inputs<number> = {
-    property_price: 0,
-    yearly_income: 0,
-    vacancy_rate: 0,
-    running_cost_rate: 0,
-    cash: 0,
-    loan: 0,
-    years: 0,
-    interest_rate: 0,
-  }
-  const errors: Errors = {}
+    const values: Inputs<number> = {
+        property_price: 0,
+        yearly_income: 0,
+        vacancy_rate: 0,
+        running_cost_rate: 0,
+        cash: 0,
+        purchase_cost: 0,
+        loan: 0,
+        years: 0,
+        interest_rate: 0,
+    }
+    const errors: Errors = {}
 
-  {
-    const [error, value] = validateNum(inputs.property_price)
-    if (error) {
-      errors.property_price = error
-    } else {
-      values.property_price = value
+    {
+        const [error, value] = validateNum(inputs.property_price)
+        if (error) {
+            errors.property_price = error
+        } else {
+            values.property_price = value
+        }
     }
-  }
-  {
-    const [error, value] = validateNum(inputs.yearly_income)
-    if (error) {
-      errors.yearly_income = error
-    } else {
-      values.yearly_income = value
+    {
+        const [error, value] = validateNum(inputs.yearly_income)
+        if (error) {
+            errors.yearly_income = error
+        } else {
+            values.yearly_income = value
+        }
     }
-  }
-  {
-    const [error, value] = validateNum(inputs.vacancy_rate, false, 0, 100)
-    if (error) {
-      errors.vacancy_rate = error
-    } else {
-      values.vacancy_rate = value
+    {
+        const [error, value] = validateNum(inputs.vacancy_rate, false, 0, 100)
+        if (error) {
+            errors.vacancy_rate = error
+        } else {
+            values.vacancy_rate = value
+        }
     }
-  }
-  {
-    const [error, value] = validateNum(inputs.running_cost_rate, false, 0, 100)
-    if (error) {
-      errors.running_cost_rate = error
-    } else {
-      values.running_cost_rate = value
+    {
+        const [error, value] = validateNum(
+            inputs.running_cost_rate,
+            false,
+            0,
+            100
+        )
+        if (error) {
+            errors.running_cost_rate = error
+        } else {
+            values.running_cost_rate = value
+        }
     }
-  }
-  {
-    const [error, value] = validateNum(inputs.cash)
-    if (error) {
-      errors.cash = error
-    } else {
-      values.cash = value
+    {
+        const [error, value] = validateNum(inputs.cash)
+        if (error) {
+            errors.cash = error
+        } else {
+            values.cash = value
+        }
     }
-  }
-  {
-    const [error, value] = validateNum(inputs.loan)
-    if (error) {
-      errors.loan = error
-    } else {
-      values.loan = value
+    {
+        const [error, value] = validateNum(inputs.purchase_cost)
+        if (error) {
+            errors.purchase_cost = error
+        } else {
+            values.purchase_cost = value
+        }
     }
-  }
-  {
-    const [error, value] = validateNum(inputs.years)
-    if (error) {
-      errors.years = error
-    } else {
-      values.years = value
+    {
+        const [error, value] = validateNum(inputs.years)
+        if (error) {
+            errors.years = error
+        } else {
+            values.years = value
+        }
     }
-  }
-  {
-    const [error, value] = validateNum(inputs.interest_rate, false, 0)
-    if (error) {
-      errors.interest_rate = error
-    } else {
-      values.interest_rate = value
+    {
+        const [error, value] = validateNum(inputs.interest_rate, false, 0)
+        if (error) {
+            errors.interest_rate = error
+        } else {
+            values.interest_rate = value
+        }
     }
-  }
 
-  // loan + cash >= property price
-  if (!(values.loan + values.cash >= values.property_price)) {
-    errors.loan = "自己資金 + 借入金額 < 物件価格"
-  }
+    if (!errors.property_price && !errors.cash && !errors.purchase_cost) {
+        const loan = calculateLoan(inputs)
+        if (loan != null) {
+            values.loan = loan
+            if (values.loan < 0) {
+                errors.cash = "自己資金 > 物件価格 + 購入時諸費用"
+            }
+        }
+    }
 
-  // if loan > 0 then years > 0
-  if (values.loan > 0 && values.years == 0) {
-    errors.years = "借入金額 > 0, 0より大きい数字を入力してください"
-  }
+    // if loan > 0 then years > 0
+    if (values.loan > 0 && values.years == 0) {
+        errors.years = "借入金額 > 0, 0より大きい数字を入力してください"
+    }
 
-  return [Object.keys(errors).length > 0 ? errors : null, values]
+    return [Object.keys(errors).length > 0 ? errors : null, values]
+}
+
+export function calculateLoan(args: {
+    property_price: string
+    purchase_cost: string
+    cash: string
+}): number | null {
+    const property_price = parseInt(args.property_price)
+    const purchase_cost = parseInt(args.purchase_cost)
+    const cash = parseInt(args.cash)
+
+    const loan = property_price + purchase_cost - cash
+
+    return isNaN(loan) ? null : loan
 }
