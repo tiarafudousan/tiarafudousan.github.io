91ca6db86a7ef7db795c5373c949f8fc3b4f338a
t4sk <tsk.nakamura@gmail.com>
2023-11-08 09:04:39 +0900

    cf table group rows

--- app/src/components/CashFlowTable.tsx
+++ app/src/components/CashFlowTable.tsx
@@ -1,22 +1,66 @@
 import React from "react"
 import { CashFlowData } from "../lib/cf"
 
+const Color: React.FC<{ num: number }> = ({ num }) => {
+  return <span className={num >= 0 ? "" : "text-red-500"}>{num}</span>
+}
+
+const RowSpan: React.FC<{
+  rowSpan: number
+  header: string
+  rows: {
+    text: string
+    get: (d: CashFlowData) => number
+  }[]
+}> = ({ rowSpan, header, rows }) => {
+  return (
+    <>
+      <tr>
+        <th rowSpan={rowSpan}>{header}</th>
+        <td>555-1234</td>
+      </tr>
+      <tr>
+        <td>555-8745</td>
+      </tr>
+    </>
+  )
+}
+
 interface Props {
   data: CashFlowData[]
 }
 
+// TODO: sticky columns - freeze first 2 columns
 const CashFlowTable: React.FC<Props> = ({ data }) => {
-  function color(x: number) {
-    return <span className={x >= 0 ? "" : "text-red-500"}>{x}</span>
-  }
-
-  function row(text: string, get: (d: CashFlowData) => number) {
+  function row({
+    rowSpan = 0,
+    header = "",
+    text,
+    get,
+    backgroundColor = "",
+  }: {
+    rowSpan?: number
+    header?: string
+    text: string
+    get: (d: CashFlowData) => number
+    backgroundColor?: string
+  }) {
     return (
-      <tr>
-        <th className="border border-slate-300 text-left px-2">{text}</th>
+      <tr className={backgroundColor}>
+        {rowSpan >= 1 ? (
+          <th
+            className="font-semibold border border-slate-300 text-left px-2 align-top whitespace-nowrap"
+            rowSpan={rowSpan}
+          >
+            {header}
+          </th>
+        ) : null}
+        <th className="font-medium border border-slate-300 text-left px-2 whitespace-nowrap">
+          {text}
+        </th>
         {data.map((d, i) => (
           <td key={i} className="border border-slate-300 px-2 text-right">
-            {color(get(d))}
+            <Color num={get(d)} />
           </td>
         ))}
       </tr>
@@ -24,10 +68,11 @@ const CashFlowTable: React.FC<Props> = ({ data }) => {
   }
 
   return (
-    <table className="border-collapse border border-slate-300">
-      <tbody>
+    <table className="text-sm border-collapse border border-slate-300 w-[600px] overflow-x-auto">
+      <thead>
         <tr>
-          <th></th>
+          <th className=""></th>
+          <th className=""></th>
           {data.map((_, i) => (
             <td key={i} className="border border-slate-300 text-center">
               {i}
@@ -35,13 +80,54 @@ const CashFlowTable: React.FC<Props> = ({ data }) => {
           ))}
           <td></td>
         </tr>
-        {row("GPI", (d) => d.gpi)}
-        {row("EGI", (d) => Math.floor(d.egi))}
-        {row("OPEX", (d) => Math.floor(d.opex))}
-        {row("NOI", (d) => Math.floor(d.noi))}
-        {row("ADS", (d) => Math.floor(d.ads))}
-        {row("BTCF", (d) => Math.floor(d.btcf))}
-        {row("ATCF", (d) => Math.floor(d.atcf))}
+      </thead>
+
+      <tbody>
+        {row({ rowSpan: 2, header: "収入", text: "GPI", get: (d) => d.gpi })}
+        {row({
+          text: "EGI",
+          get: (d) => Math.floor(d.egi),
+          backgroundColor: "bg-blue-100",
+        })}
+
+        {row({
+          rowSpan: 4,
+          header: "費用",
+          text: "固定資産税",
+          get: (d) => Math.floor(d.property_tax),
+        })}
+        {row({
+          text: "都市計画税 ",
+          get: (d) => Math.floor(d.city_planning_tax),
+        })}
+        {row({
+          text: "管理費",
+          get: (d) => Math.floor(d.maintanence_fee),
+        })}
+        {/* TODO: ad fee, restoration fee, insurance fee, opex misc fee, initial cost */}
+        {row({
+          text: "OPEX",
+          get: (d) => Math.floor(d.opex),
+          backgroundColor: "bg-blue-100",
+        })}
+
+        {row({
+          rowSpan: 3,
+          header: "BTCF",
+          text: "NOI",
+          get: (d) => Math.floor(d.noi),
+        })}
+        {row({
+          text: "ADS",
+          get: (d) => Math.floor(d.ads),
+        })}
+        {row({ text: "BTCF", get: (d) => Math.floor(d.btcf) })}
+        {row({
+          rowSpan: 1,
+          header: "ATCF",
+          text: "ATCF",
+          get: (d) => Math.floor(d.atcf),
+        })}
       </tbody>
     </table>
   )
